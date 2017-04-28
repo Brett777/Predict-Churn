3160d6bb5d517ccd4a0ca30691a9f4a613d1c3da
Brett777 <brett@datascience.com>
2017-04-28 15:45:38 +0000

    Sync from DataScience jupyter

--- DeployEnsemble.py
+++ DeployEnsemble.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from tabulate import tabulate
 # initialize the model scoring server
-h2o.init(nthreads=2,max_mem_size=2, start_h2o=True, strict_version_check = False)
+h2o.init(nthreads=2,max_mem_size=1, start_h2o=True, strict_version_check = False)
 
 # function to get files from s3
 def pull_file_from_s3(key):
