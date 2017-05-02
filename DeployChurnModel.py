202bdbabdb4f59e3c6c8a643875733aeb4da34b3
Brett777 <brett@datascience.com>
2017-05-02 10:22:56 -0400

    Add files via upload

---
+++ DeployChurnModel.py
@@ -0,0 +1,66 @@
+import os
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import h2o
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+
+# initialize the model scoring server
+h2o.init(nthreads=1,max_mem_size=1, start_h2o=True, strict_version_check = False)
+
+# function to get files from s3
+def pull_file_from_s3(key):
+    def get_bucket():            
+        access='AKIAJXTIXBAJVFOPYSLQ' 
+        secret='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'
+        customer = 'demonstration'
+        conn = S3Connection(access,secret)
+        b = conn.get_bucket('dsclouddata',validate=False)
+        return b
+
+    s3_bucket = get_bucket()
+    payload = s3_bucket.get_key(key)
+    local_file = payload.get_contents_to_filename(key)
+    return key
+
+    
+# download the model from s3
+downloaded_model = pull_file_from_s3('gbm_grid_binomial_model_1')  
+
+def churn_predict(State,AccountLength,AreaCode,Phone,IntlPlan,VMailPlan,VMailMessage,DayMins,DayCalls,DayCharge,EveMins,EveCalls,EveCharge,NightMins,NightCalls,NightCharge,IntlMins,IntlCalls,IntlCharge,CustServCalls):
+    # connect to the model scoring service
+    h2o.connect()
+
+    # open the downloaded model
+    ChurnPredictor = h2o.load_model(path=downloaded_model)  
+
+    # define a feature vector to evaluate with the model
+    newData = pd.DataFrame({'State' : State,
+                            'Account Length' : AccountLength,
+                            'Area Code' : AreaCode,
+                            'Phone' : Phone,
+                            'Int\'l Plan' : IntlPlan,
+                            'VMail Plan' : VMailPlan,
+                            'VMail Message' : VMailMessage,
+                            'Day Mins' : DayMins,
+                            'Day Calls' : DayCalls,
+                            'Day Charge' : DayCharge,
+                            'Eve Mins' : EveMins,
+                            'Eve Calls' : EveCalls,
+                            'Eve Charge' : EveCharge,
+                            'Night Mins' : NightMins,
+                            'Night Calls' : NightCalls,
+                            'Night Charge' : NightCharge,
+                            'Intl Mins' :IntlMins,
+                            'Intl Calls' : IntlCalls,
+                            'Intl Charge' : IntlCharge,
+                            'CustServ Calls' : CustServCalls}, index=[0])
+    
+    # evaluate the feature vector using the model
+    predictions = ChurnPredictor.predict(h2o.H2OFrame(newData))
+    predictionsOut = h2o.as_list(predictions, use_pandas=False)
+    prediction = predictionsOut[1][0]
+    probabilityChurn = predictionsOut[1][1]
+    probabilityRetain = predictionsOut[1][2]
+    return "Prediction: " + str(prediction) + " |Probability to Churn: " + str(probabilityChurn) + " |Probability to Retain: " + str(probabilityRetain)
