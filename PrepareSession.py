a57fdc79cd790f39b09a96ffa93f3d0ac4915d04
Brett777 <brett@datascience.com>
2017-05-03 19:07:58 +0000

    Sync from DataScience jupyter

--- PrepareSession.py
+++ PrepareSession.py
@@ -1,9 +1,4 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-get_ipython().system(' sudo apt-get update')
-get_ipython().system(' sudo apt-get install openjdk-8-jre -y')
-get_ipython().system(' pip install http://h2o-release.s3.amazonaws.com/h2o/rel-ueno/5/Python/h2o-3.10.4.5-py2.py3-none-any.whl')
-
+import os
+os.system("sudo apt-get update")
+os.system('sudo apt-get install openjdk-8-jre -y')
+os.system('pip install http://h2o-release.s3.amazonaws.com/h2o/rel-ueno/5/Python/h2o-3.10.4.5-py2.py3-none-any.whl')
