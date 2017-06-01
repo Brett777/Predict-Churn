15b72b2d2beec2c7ebb44f8d26315f31f01d9c6f
Brett777 <brett@datascience.com>
2017-06-01 01:26:56 +0000

    Sync from DataScience jupyter

--- new-requirements-py.py
+++ new-requirements-py.py
@@ -1,4 +1,4 @@
 pandas
 numpy
 boto
-pip install http://h2o-release.s3.amazonaws.com/h2o/master/3899/Python/h2o-3.11.0.3899-py2.py3-none-any.whl
+http://h2o-release.s3.amazonaws.com/h2o/master/3899/Python/h2o-3.11.0.3899-py2.py3-none-any.whl
